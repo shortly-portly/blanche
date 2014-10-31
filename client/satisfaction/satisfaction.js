3279225c8045bbf7e960ce18935cf2a2e1eb6bce
David Simmons <shortlyportly@googlemail.com>
2014-10-31 23:08:05 +0000

    Added Bootstrap 3 and a bit of colour

---
+++ client/satisfaction/satisfaction.js
@@ -0,0 +1,35 @@
+Template.satisfaction.rendered = function() {
+var data = {
+    labels: ["Work", "Money", "Health", "Friends & Family", "Fun/Recreation"],
+    datasets: [
+        {
+            label: "My First dataset",
+            fillColor: "rgba(220,220,220,0.2)",
+            strokeColor: "rgba(215, 31, 44, 0.9);",
+            pointColor: "rgba(220,220,220,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(220,220,220,1)",
+            data: [0,0,0,0,0]
+        }
+    ]
+};
+  var options = {scaleShowLabels : true,
+  pointLabelFontSize: 12};
+
+  Chart.defaults.global.scaleOverride = true;
+  Chart.defaults.global.scaleSteps = 10;
+  Chart.defaults.global.scaleStepWidth = 1;
+
+  ctx = document.getElementById("myChart").getContext("2d");
+  myRadarChart = new Chart(ctx).Radar(data, options);
+
+
+};
+
+Template.satisfaction.events({
+  'click': function(evt) {
+    var activePoints = myRadarChart.getValue(evt);
+  
+  }
+});
